1623cb184e9468dadbfdfd1d883f4f39b394c9f7
Parzival Legmaier <legmaier@gmx.at>
2016-10-29 13:20:44 +0200

    Added:  - Procedural Plane Script
    Updated:
     - Resized Render Textures to 2048

---
+++ Assets/Editor/PlaneGeneratorEditor.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(PlaneGenerator))]
+public class PlaneGeneratorEditor : Editor
+{
+    // Reference to the PlaneGenerator
+    private PlaneGenerator planeGenerator;
+
+    // Serialized version of the plane generator
+    private SerializedObject serializedPlaneGenerator;
+
+    // Styles
+    private GUIStyle titleStyle;
+
+    /// <summary>
+    /// Called everytime the inspected object is selected.
+    /// </summary>
+    private void OnEnable()
+    {
+        planeGenerator = (PlaneGenerator)target;
+        serializedPlaneGenerator = new SerializedObject(planeGenerator);
+
+        InitStyles();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawSettings();
+        DrawDebugSettings();
+        DrawButton();
+
+        if (GUI.changed)
+            EditorUtility.SetDirty(planeGenerator);
+
+        serializedPlaneGenerator.ApplyModifiedProperties();
+    }
+
+    private void DrawButton()
+    {
+        if (GUILayout.Button("Generate Mesh!", GUILayout.Height(Screen.height * 0.03f)))
+        {
+            planeGenerator.GenerateMesh();
+        }
+    }
+
+    private void DrawDebugSettings()
+    {
+        SerializedProperty debugDraw = serializedPlaneGenerator.FindProperty("debugDrawVertex");
+        EditorGUILayout.BeginVertical("box");
+        EditorGUILayout.LabelField("Debug", titleStyle);
+        EditorGUILayout.PropertyField(debugDraw);
+        EditorGUILayout.EndVertical();
+
+    }
+
+    private void DrawSettings()
+    {
+        // Title
+        SerializedProperty serializedX = serializedPlaneGenerator.FindProperty("xSize");
+        SerializedProperty serializedY = serializedPlaneGenerator.FindProperty("ySize");
+        SerializedProperty serializedVertexOffset = serializedPlaneGenerator.FindProperty("vertexOffset");
+        SerializedProperty serializedMeshMaterial = serializedPlaneGenerator.FindProperty("meshMaterial");
+
+
+        EditorGUILayout.BeginVertical("box");
+        EditorGUILayout.LabelField("Mesh settings", titleStyle);
+        EditorGUILayout.PropertyField(serializedX);
+        EditorGUILayout.PropertyField(serializedY);
+        EditorGUILayout.PropertyField(serializedVertexOffset);
+        EditorGUILayout.PropertyField(serializedMeshMaterial);
+        EditorGUILayout.EndVertical();
+    }
+
+    /// <summary>
+    /// Inits the styles.
+    /// </summary>
+    private void InitStyles()
+    {
+        titleStyle = new GUIStyle();
+        titleStyle.alignment = TextAnchor.MiddleCenter;
+        titleStyle.fontSize = 15;
+        titleStyle.fontStyle = FontStyle.Bold;
+    }
+}
---
+++ Assets/Scripts/MeshGenerator/PlaneGenerator.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class PlaneGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private int xSize = 4;
+
+    [SerializeField]
+    private int ySize = 4;
+
+    [SerializeField]
+    private float vertexOffset = 0.2f;
+
+    [SerializeField]
+    private Material meshMaterial;
+
+    [SerializeField]
+    private bool debugDrawVertex = false;
+
+    #region Internal members
+    private Vector3[] vertices;
+    private Vector2[] uv;
+    private Vector4[] tangents;
+    private Mesh mesh;
+    #endregion
+
+    public void GenerateMesh()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        mesh.name = "Procedural Plane";
+
+        GetComponent<Renderer>().material = meshMaterial;
+        
+        GenerateVerticesUvTangents();
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.tangents = tangents;
+        mesh.triangles = CalculateTriangles();
+        mesh.RecalculateNormals();
+    }
+
+    private void GenerateVerticesUvTangents()
+    {
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        uv = new Vector2[vertices.Length];
+        tangents = new Vector4[vertices.Length];
+        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+
+        float yOffset = 0f;
+        float xOffset = 0f;
+        for (int i = 0, y = 0; y <= ySize; y++, yOffset += vertexOffset)
+        {
+            xOffset = 0f;
+            for (int x = 0; x <= xSize; x++, i++, xOffset += vertexOffset)
+            {
+                vertices[i] = new Vector3(xOffset, yOffset);
+                uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+                tangents[i] = tangent;
+            }
+        }
+    }
+
+    private int[] CalculateTriangles()
+    {
+        int[] triangles = new int[xSize * ySize * 6];
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+            }
+        }
+        return triangles;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (debugDrawVertex && vertices != null)
+        {
+            Gizmos.color = Color.red;
+            for (int i = 0; i < vertices.Length; i++)
+                Gizmos.DrawCube(vertices[i], Vector3.one * 0.05f);
+        }
+    }
+}
