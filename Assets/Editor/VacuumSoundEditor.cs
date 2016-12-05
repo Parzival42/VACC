87a3d8f216fd6e813bd6b5a761f0b58bf798ec87
urstruktur <s1310238052@students.fh-hagenberg.at>
2016-12-05 03:51:07 +0100

    dustsucker is now annoyingly noisy

---
+++ Assets/Editor/VacuumSoundEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(VacuumSound))]
+class VacuumSoundEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        VacuumSound script = (VacuumSound)target;
+        if (GUILayout.Button("Start Engine"))
+        {
+            script.StartEngine();
+        }
+
+        if (GUILayout.Button("Stop Engine"))
+        {
+            script.StopEngine();
+        }
+
+    }
+}
+
---
+++ Assets/Scripts/Audio/VacuumCollisionSounds.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+class VacuumCollisionSounds : MonoBehaviour
+{
+    void OnCollisionStay(Collision col)
+    {
+        float magnitude = col.relativeVelocity.magnitude;
+    }
+}
+
