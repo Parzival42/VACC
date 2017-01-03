29e73a266b0fe6227073db9d2629c3fd83551f3b
Parzival Legmaier <legmaier@gmx.at>
2017-01-03 16:41:37 +0100

    Added PolyPictures.

---
+++ Assets/Scripts/Util/PhysicsSleepOnStart.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Forces the gameobject to sleep (physics) on start.
+/// So it won't fall of the wall for example.
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class PhysicsSleepOnStart : MonoBehaviour
+{
+    private Rigidbody rigid;
+
+	private void Start ()
+    {
+        rigid = GetComponent<Rigidbody>();
+        rigid.Sleep();
+	}
+}
