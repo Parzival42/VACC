4b432b230f902af3eee9edff4fecdee51cb4fad1
urstruktur <s1310238052@students.fh-hagenberg.at>
2017-01-04 16:33:01 +0100

    added ceiling fan

---
+++ Assets/Scripts/Util/RotateScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour {
+
+    [Range(1, 600)]
+    public float speed = 220f;
+
+	void Update () {
+        transform.Rotate(Vector3.up * Time.deltaTime * speed);
+    }
+}
