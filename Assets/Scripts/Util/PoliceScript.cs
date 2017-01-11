c39ae7e82043d4a04e15d90777184b854208ecaa
urstruktur <s1310238052@students.fh-hagenberg.at>
2017-01-11 15:01:40 +0100

    new hoover model + police gobo + ssao tweaked

---
+++ Assets/Scripts/Util/PoliceScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceScript : MonoBehaviour {
+
+    public bool active = true;
+
+    public GameObject light1;
+    public GameObject light2;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (true)
+        {
+            Vector3 newPosition = this.transform.position;
+            newPosition.z -= Time.deltaTime * 6;
+            this.transform.position = newPosition;
+        }
+	}
+}
