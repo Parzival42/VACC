2c4753d120f41229ee7d0b56c9dbaed3af653f33
Ju Lia <jul.ya@gmx.at>
2017-02-02 22:30:42 +0100

    collision sounds!

---
+++ Assets/Scripts/Audio/CollisionSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSound : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string coll = "event:/Collision";
+    FMOD.Studio.EventInstance collEvent;
+
+    public string collisionSound = "wood";
+    FMOD.Studio.ParameterInstance collParam;
+
+    void OnCollisionEnter()
+    {
+        collEvent = FMODUnity.RuntimeManager.CreateInstance(coll);
+        collEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        collEvent.getParameter(collisionSound, out collParam);
+        collParam.setValue(1f);
+        collEvent.start();
+        collEvent.release();
+    }
+
+}
