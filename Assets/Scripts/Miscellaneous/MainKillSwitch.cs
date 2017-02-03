cd66147d6a7a673bad6f430841e85c62c9054571
Andreas Pointner <pointner.andy@gmail.com>
2017-02-03 23:55:38 +0100

    beginnings of a global power kill switch

---
+++ Assets/Scripts/Miscellaneous/MainKillSwitch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void MainPowerHandler(bool powerOn);
+
+public class MainKillSwitch : MonoBehaviour {
+
+
+    public static event MainPowerHandler MainPowerChanged;
+    private bool powerOn = false;
+
+	// Use this for initialization
+	void Start () {
+        //OnMainPowerChanged(false);
+	}
+
+    void OnMouseDown()
+    {
+        powerOn = !powerOn;
+        OnMainPowerChanged(powerOn);
+        Debug.Log("done");
+    }
+
+    private void OnMainPowerChanged(bool powerOn)
+    {
+        if(MainPowerChanged != null)
+        {
+            MainPowerChanged(powerOn);
+        }
+    }
+}
