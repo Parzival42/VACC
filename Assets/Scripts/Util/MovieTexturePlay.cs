57076a6d2c5c6c0d37ac0673a232528afaa0cdf9
urstruktur <s1310238052@students.fh-hagenberg.at>
2017-02-03 01:05:19 +0100

    added movie texture

---
+++ Assets/Scripts/Util/MovieTexturePlay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovieTexturePlay : MonoBehaviour {
+
+    public MovieTexture movTexture;
+
+    void Start () {
+        if(GetComponent<Renderer>().materials[1].mainTexture is MovieTexture){
+            movTexture = (MovieTexture)GetComponent<Renderer>().materials[1].mainTexture;
+            movTexture.loop = true;
+            movTexture.Play();
+        }
+    }
+}
