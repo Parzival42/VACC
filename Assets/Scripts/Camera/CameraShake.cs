e1b5a828dcc730953d766da6cd4a9537395f66b1
Parzival Legmaier <legmaier@gmx.at>
2017-02-04 19:02:58 +0100

    Added Image Based Camera Shake Added camera shake in Animation Manager.

---
+++ Assets/Scripts/Camera/CameraShake.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraShake : MonoBehaviour
+{
+    [SerializeField]
+    protected Shader currentShader;
+
+    [Header("Shader Parameters")]
+    [SerializeField]
+    public float offsetX;
+
+    [SerializeField]
+    public float offsetY;
+
+    [SerializeField]
+    public float zoom;
+
+    private Material currentMaterial;
+    public Material material
+    {
+        get
+        {
+            if (currentMaterial == null)
+            {
+                currentMaterial = new Material(currentShader);
+                currentMaterial.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return currentMaterial;
+        }
+    }
+
+    private void Start()
+    {
+        if (!SystemInfo.supportsImageEffects)
+            enabled = false;
+        else if (!currentShader && !currentShader.isSupported)
+            enabled = false;
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (currentShader != null)
+        {
+            material.SetFloat("_OffsetX", offsetX);
+            material.SetFloat("_OffsetY", offsetY);
+            material.SetFloat("_Zoom", zoom);
+            Graphics.Blit(source, destination, material);
+        }
+        else
+            Graphics.Blit(source, destination);
+    }
+
+    private void OnDisable()
+    {
+        if (currentMaterial)
+            DestroyImmediate(currentMaterial);
+    }
+}
