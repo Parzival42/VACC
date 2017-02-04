6accbbef3e1c837a42b5447a52856e3e5b1280f1
Parzival Legmaier <legmaier@gmx.at>
2017-02-05 00:50:37 +0100

    Adjusted CarLight Animation to the sound Added shadow wall for car light.

---
+++ Assets/Scripts/StoryScripts/CarLightStarter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CarLightStarter : MonoBehaviour
+{
+	private void Start ()
+    {
+        StartCoroutine(PlayCarLight());
+	}
+
+    private IEnumerator PlayCarLight()
+    {
+        yield return new WaitForSeconds(10f);
+        AnimationManager.Instance.PlayCarLight();
+    }
+}
